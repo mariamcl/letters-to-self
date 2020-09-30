be43dc3afc2b7b921abd362629b5b92fd8d3a230
Maria McLaughlin <mariamclaughlin@college.harvard.edu>
2020-09-29 21:05:07 -0400

    First Commit

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>How was your day?</Text>
+      <TextInput
+        style={{
+          height: 40,
+          width: 300,
+          borderColor: 'gray',
+          borderWidth: 1,
+          marginBottom: 10
+        }}
+        defaultValue=" Scale of 1 to 10"
+       />
+      <Button title="Enter" onPress={(e) => console.log(e)}/>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +29,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    marginBottom: 10
+  }
 });
